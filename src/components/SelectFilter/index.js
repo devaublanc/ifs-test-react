96f4fdfec0e460d6e2b5e849ab929027d5183521
Tawny <tony.malto.simon@gmail.com>
2017-09-29 10:19:10 +0200

    bug dans render()

--- src/components/SelectFilter/index.js
+++ src/components/SelectFilter/index.js
@@ -3,8 +3,16 @@ import classNames from 'classnames'
 
 import styles from './index.css'
 
+import Category from 'components/Category'
 
 export default class SelectFilter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filtersToRender: this.props.filters
+        }
+    }
 
     static propTypes = {
         filters: PropTypes.arrayOf(PropTypes.shape({
@@ -15,14 +23,58 @@ export default class SelectFilter extends Component {
         rootStyle: PropTypes.string
     }
 
+    componentWillReceiveProps(nextProps) {
+        console.log('component received props')
+        if (nextProps) {
+            this.setState({
+                filtersToRender: nextProps.filters,
+            });
+        }
+    }
+
+    renderCategories() {
+        console.log(this.state.filtersToRender)
+        var Filters = this.state.filtersToRender.map((filter, index) =>
+             <Category key={index.toString()} filter={filter.name} children={filter.children} />
+        );
+        return Filters;
+
+    }
+
+    filterFilters(pattern) {
+        const regexString = '^' + pattern
+        const regex = new RegExp(regexString, 'i')
+        // removes categories where no child matches
+        let newCategories = this.props.filters.filter(filter => {
+            return filter.children.some(child => regex.test(child))
+        })
+        // removes non-matching children from categories
+        return newCategories.map(filter => {
+            let newCategory = Object.assign({}, filter)
+            newCategory.children = filter.children.filter(child => regex.test(child))
+            return newCategory
+        })
+
+    }
+
+    handleChange(e) {
+        const inputVal = e.target.value;
+        const filteredFilters = this.filterFilters(inputVal)
+        this.setState((prevState) => ({
+        filtersToRender: filteredFilters,
+        }));
+    }
+
     render() {
         const {
             rootStyle
         } = this.props;
-
+        console.log("RENDERED CATEGORIES",this.renderCategories())
         return (
             <div className={ classNames(styles.selectfilter, rootStyle)}>
-                Code your Component here
+                <p>select a channel</p>
+                <input type="text" onChange={this.handleChange.bind(this)} />
+                {this.renderCategories()}
             </div>
         )
     }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -10,7 +10,11 @@ import styles from './index.css'
 export default class Home extends Component {
 
     state = {
-        data: null
+        data: []
+    }
+
+    handleChange(currentFilter) {
+
     }
 
     componentDidMount() {
@@ -21,19 +25,37 @@ export default class Home extends Component {
             .end((err, res) => {
                 if (res) {
                     console.log('res', res.body) // eslint-disable-line
+                    // change state when arrives and then pass it to selectfilter
+                    this.setState({
+                        data: this.formatData(res.body),
+                    });
                 }
             });
+
     }
 
-    render() {
+    formatData(data) {
+        let flatArrayFromCategoryNames = [];
+        let formatted = data;
+        formatted.forEach(category => {
+            let children = category.children.forEach(child => flatArrayFromCategoryNames.push(child.name))
+            category.children = flatArrayFromCategoryNames;
+            flatArrayFromCategoryNames = [];
+        })
+        return formatted
+    }
 
+
+
+    render() {
         return (
             <div className={ styles.home }>
                 <h1>ifs test react</h1>
                 <SelectFilter
-                    filters={[]}
+                    filters={this.state.data}
                     onChange={(currentFilter) => {
                         console.log('currentFilter', currentFilter) // eslint-disable-line
+
                     }}
                     rootStyle={ styles.filter }
                 />
